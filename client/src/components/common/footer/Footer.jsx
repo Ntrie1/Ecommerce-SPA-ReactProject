52febc47af3c36b43b757ee4b69c522f07421f9d
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-11-17 15:53:46 +0200

    Footer added, Header edited

--- client/src/components/common/footer/Footer.jsx
+++ client/src/components/common/footer/Footer.jsx
@@ -1,5 +1,11 @@
+import styles from './Footer.module.css'
+
 const Footer = () =>{
     return(
-        <div>footer</div>
+        <div className={styles.footer}>
+        <p>Copyright &copy; 2023 <span className={styles.title}>ElectraSwap</span>, All rights reserved.</p>
+        </div>
     )
 }
+
+export default Footer;
--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -25,13 +25,13 @@ function classNames(...classes) {
 
 const Header = () => {
   return (
-    <Disclosure as="nav" className="bg-pink-800">
+    <Disclosure as="nav" className={styles.nav}>
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -56,7 +56,7 @@ const Header = () => {
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -81,8 +81,8 @@ const Header = () => {
                         key={item.name}
                         to={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                          item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-bold'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -103,7 +103,7 @@ const Header = () => {
                   as={Link}
                   to={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white' : 'text-black-300 hover:bg-gray-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
