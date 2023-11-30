f7ba9a48b26b8e1dd7ac2121807b4bc5fbb4258d
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-11-30 18:02:31 +0200

    useFrom custom hook edited

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,26 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
+import * as Yup from "yup";
 
-export default function useForm(submitHandler, initalValues) {
-     const [values, setValues] = useState(initalValues);
+export default function useForm(submitHandler, initialValues, validationSchema) {
+    const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
 
+    const validateField = async (name, value) => {
+        try {
+            await validationSchema.validateAt(name, values);
+            setErrors((prevErrors) => ({ ...prevErrors, [name]: undefined }));
+        } catch (error) {
+            setErrors((prevErrors) => ({ ...prevErrors, [name]: error.message }));
+        }
+    };
+    
 
-     const onChange = (e) => {
-        setValues(state => ({
-            ...state,
-            [e.target.name]: e.target.value
-        }))
-     }
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setValues((prevValues) => ({ ...prevValues, [name]: value }));
+        validateField(name, value);
+    };
 
-     const onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
-        submitHandler(values);
-     }
+        console.log(values);
+        validationSchema
+            .validate(values, { abortEarly: false })
+            .then(() => {
+                setErrors({});
+                submitHandler(values);
+            })
+            .catch((validationErrors) => {
+                const newErrors = {};
+                validationErrors.inner.forEach((error) => {
+                    newErrors[error.path] = error.message;
+                });
+                setErrors(newErrors);
+            });
+    };
 
-
-    return { 
+    return {
         values,
+        errors,
         onChange,
-        onSubmit
-    }
+        onSubmit,
+    };
 }
