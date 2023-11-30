c78d8a589399db75c84f296b9d5b0381210333ca
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-11-30 18:05:12 +0200

    validations added

--- client/src/components/user/login/Login.jsx
+++ client/src/components/user/login/Login.jsx
@@ -1,91 +1,114 @@
 import { Link } from "react-router-dom";
-
+import * as Yup from 'yup';  // Import Yup
 import useForm from '../../../hooks/useForm';
 
 import styles from './Login.module.css'
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../../../context/authContext";
 
-const Login = () =>{
+// Define a Yup schema for validation
+const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required').min(5, 'Password must have at least 5 characters'),
+});
 
-    const { loginSubmitHandler } = useContext(AuthContext)
-    const {values, onChange, onSubmit} = useForm(loginSubmitHandler,{
-        email: '',
-        password: '',  
-    });
+const Login = () => {
+    const { loginSubmitHandler, loginError, clearError } = useContext(AuthContext)
 
-
-    return(
-        <main className={styles.mainBackground}>
-        <section className="h-screen flex items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Left column container with background */}
-          <div className="hidden md:block md:col-span-1 lg:col-span-2">
-            <img
-              src="images/login-image.png"
-              className="w-full h-full object-cover"
-              alt="Sample image"
-              />
-          </div>
-      
-          {/* Right column container */}
-          <div className="md:col-span-1 lg:col-span-1">
-            <form onSubmit={onSubmit} className="max-w-md mx-auto">
-              {/* Sign in section */}
-              <div className="mb-6">
-                {/* Email input */}
-                <input
-                  type="email"
-                  placeholder="Email address"
-                  name="email"
-                  onChange={onChange}
-                  value={values.email}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
-              </div>
-      
-              {/* Password input */}
-              <div className="mb-6">
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  onChange={onChange}
-                  value={values.password}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
-              </div>
-      
-           
-      
-              {/* Login button */}
-              <div className="text-center">
-                <button
-                  type="submit"
-                  className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
-                  >
-                  Login
-                </button>
-              </div>
-      
-              {/* Register link */}
-              <p className="mt-4 text-sm">
-                Don't have an account?{" "}
-                {/* <a
-                  href="#!"
-                  className="text-blue-500 hover:underline"
-                  >
-                  Register
-                </a> */}
-
-                <Link to={'/register'}> <span className={styles.registerButton}>Register</span></Link>
-              </p>
-            </form>
-          </div>
-        </div>
-      </section>
-    </main>
+    // Pass the validation schema to the useForm hook
+    const { values, errors, onChange, onSubmit } = useForm(
+        loginSubmitHandler,
+        {
+            email: '',
+            password: '',
+        },
+        validationSchema  // Add validation schema
     );
-}
 
- export default Login;
+    useEffect(() => {
+      return () => {clearError()}
+     },[])
+
+    return (
+        <main className={styles.mainBackground}>
+            <section className="h-screen flex items-center justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {/* Left column container with background */}
+                    <div className="hidden md:block md:col-span-1 lg:col-span-2">
+                        <img
+                            src="images/login-image.png"
+                            className="w-full h-full object-cover"
+                            alt="Sample image"
+                        />
+                    </div>
+
+                    {/* Right column container */}
+                    <div className="md:col-span-1 lg:col-span-1">
+                        <form onSubmit={onSubmit} className="max-w-md mx-auto">
+                            {/* Sign in section */}
+                            <div className="mb-6">
+                                {/* Email input */}
+                                <input
+                                    type="email"
+                                    placeholder="Email address"
+                                    name="email"
+                                    onChange={onChange}
+                                    value={values.email}
+                                    className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${
+                                        errors.email ? 'border-red-500' : ''
+                                    }`}
+                                />
+                                {errors.email && (
+                                    <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+                                )}
+                            </div>
+
+                            {/* Password input */}
+                            <div className="mb-6">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    onChange={onChange}
+                                    value={values.password}
+                                    className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${
+                                        errors.password ? 'border-red-500' : ''
+                                    }`}
+                                />
+                                {errors.password && (
+                                    <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+                                )}
+                            </div>
+
+                            {/* Login button */}
+                            {loginError && (
+                   <div>
+                    <p className='text-red-600 text-sm mt-1 mb-4'>{loginError} Please try again.</p>
+                </div>
+            )}
+                            <div className="text-center">
+                                <button
+                                    type="submit"
+                                    className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
+                                >
+                                    Login
+                                </button>
+                            </div>
+
+                            {/* Register link */}
+                            <p className="mt-4 text-sm">
+                                Don't have an account?{' '}
+                                <Link to={'/register'}>
+                                    {' '}
+                                    <span className={styles.registerButton}>Register</span>
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default Login;
--- client/src/components/user/register/Register.jsx
+++ client/src/components/user/register/Register.jsx
@@ -1,127 +1,126 @@
-import { Link } from "react-router-dom";
-
-import styles from './Register.module.css'
-import { useContext } from "react";
-import AuthContext from "../../../context/authContext";
-import useForm from "../../../hooks/useForm";
+// Register.js
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+import styles from './Register.module.css';
+import { useContext, useEffect } from 'react';
+import AuthContext from '../../../context/authContext';
+import useForm from '../../../hooks/useForm';
 
 const Register = () => {
-    const { registerSubmitHandler } = useContext(AuthContext);
-    const {values, onChange, onSubmit} = useForm(registerSubmitHandler,{
-        name: '',
-        email: '',
-        username: '',
-        password: '',
-        rePassword: '',
-    })
+  const { registerSubmitHandler, registerError, clearError } = useContext(AuthContext);
 
-    return(
-        <main className={styles.mainBackground}>
-        <section className="h-screen flex items-center justify-center">
+  
+
+  // Define the validation schema
+  const registrationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    username: Yup.string().required('Username is required').min(4, 'Name should be at least 4 characters'),
+    password: Yup.string().min(5, 'Password must be at least 5 characters').required('Password is required'),
+    repeatPassword: Yup.string()
+    .required('Confirm Password is required')
+  });
+
+  const { values, errors, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    email: '',
+    username: '',
+    password: '',
+    repeatPassword: '',
+  }, registrationSchema);
+
+  
+  useEffect(() => {
+    return () => {clearError()}
+   },[])
+
+  return (
+    <main className={styles.mainBackground}>
+      <section className="h-screen flex items-center justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Left column container with background */}
           <div className="hidden md:block md:col-span-1 lg:col-span-2">
-            <img
-              src="images/register-image.png"
-              className="w-3/5 h-auto object-cover"
-              alt="Sample image"
-              />
+            <img src="images/register-image.png" className="w-3/5 h-auto object-cover" alt="Sample image" />
           </div>
-      
-          {/* Right column container */}
           <div className="md:col-span-1 lg:col-span-1">
             <form onSubmit={onSubmit} className="max-w-md mx-auto">
-              {/* Sign in section */}
-              <div className="mb-6">
-                {/* Name input */}
+              {/* <div className="mb-6">
                 <input
                   type="text"
                   placeholder="Name"
                   name="name"
                   onChange={onChange}
                   value={values.name}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
-              </div>
-
-              
+                  className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${errors.name ? 'border-red-500' : ''}`}
+                />
+                {errors.name && <p className="text-red-600 text-sm mt-1">{errors.name}</p>}
+              </div> */}
               <div className="mb-6">
-                {/* Email input */}
                 <input
                   type="email"
                   placeholder="Email address"
                   name="email"
                   onChange={onChange}
                   value={values.email}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
+                  className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${errors.email ? 'border-red-500' : ''}`}
+                />
+                {errors.email && <p className="text-red-600 text-sm mt-1">{errors.email}</p>}
               </div>
-
               <div className="mb-6">
-                {/* Username input */}
                 <input
                   type="text"
                   placeholder="Username"
                   name="username"
                   onChange={onChange}
                   value={values.username}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
+                  className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${errors.username ? 'border-red-500' : ''}`}
+                />
+                {errors.username && <p className="text-red-600 text-sm mt-1">{errors.username}</p>}
               </div>
-      
-              {/* Password input */}
               <div className="mb-6">
                 <input
                   type="password"
-                  name="password"
                   placeholder="Password"
+                  name="password"
                   onChange={onChange}
                   value={values.password}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
+                  className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${errors.password ? 'border-red-500' : ''}`}
+                />
+                {errors.password && <p className="text-red-600 text-sm mt-1">{errors.password}</p>}
               </div>
-
               <div className="mb-6">
                 <input
                   type="password"
-                  name="rePassword"
                   placeholder="Confirm Password"
+                  name="repeatPassword"
                   onChange={onChange}
-                  value={values.rePassword}
-                  className="w-full p-3 border border-solid border-gray-300 outline-none rounded-lg"
-                  />
+                  value={values.repeatPassword}
+                  className={`w-full p-3 border border-solid border-gray-300 outline-none rounded-lg ${errors.repeatPassword ? 'border-red-500' : ''}`}
+                />
+                {errors.repeatPassword && <p className="text-red-600 text-sm mt-1">{errors.repeatPassword}</p>}
               </div>
-      
-           
-      
-              {/* Login button */}
+              {registerError && (
+                   <div>
+                    <p className='text-red-600 text-sm mt-1 mb-4'>{registerError} Please try again.</p>
+                </div>
+            )}
               <div className="text-center">
                 <button
                   type="submit"
                   className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
-                  >
+                >
                   Register
                 </button>
               </div>
-      
-              {/* Register link */}
               <p className="mt-4 text-sm">
-                Have an account already?{" "}
-                {/* <a
-                  href="#!"
-                  className="text-blue-500 hover:underline"
-                  >
-                  Register
-                </a> */}
-
-                 <Link to={'/login'}> <span className={styles.loginButton}>Login</span></Link>
+                Have an account already?{' '}
+                <Link to={'/login'}>
+                  <span className={styles.loginButton}>Login</span>
+                </Link>
               </p>
             </form>
           </div>
         </div>
       </section>
     </main>
-    );
-}
+  );
+};
 
 export default Register;
