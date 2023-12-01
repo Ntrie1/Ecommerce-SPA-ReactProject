65fdd59afefd57d3a09a114ee8789283fa4697fc
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-12-01 14:49:27 +0200

    fetch func for get all devices added

--- client/src/services/devicesService.js
+++ client/src/services/devicesService.js
@@ -4,6 +4,12 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3000/api/devices';
 
 
+export const getAll = async () => {
+    const response = await request.get(`${baseUrl}`)
+
+    return response;
+}
+
 export const create = async (data) => {
     const response = await request.post(`${baseUrl}/create`, data);
 
