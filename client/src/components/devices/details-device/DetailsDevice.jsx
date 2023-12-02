959935b921ee5f2ca0d6c0bebe4906f2ceb9a930
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-12-02 16:14:31 +0200

    css added plus error handling in the component

--- client/src/components/devices/details-device/DetailsDevice.jsx
+++ client/src/components/devices/details-device/DetailsDevice.jsx
@@ -1,9 +1,56 @@
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../../context/authContext";
+import { useParams } from "react-router-dom";
+
+import styles from './DetailsDevice.module.css'
+import * as deviceService from '../../../services/devicesService'
+
 const DetailsDevice = () => {
+    const { userId } = useContext(AuthContext);
+    const { deviceId } = useParams();
+    const [device, setDevice] = useState([]);
+    const [isOwner, setIsOwner] = useState(false);
+    const [error, setError] = useState(null);
+
+   
+
+    useEffect(() => {
+        let isMounted = true;
+
+        deviceService.getOne(deviceId)
+            .then((result) => {
+                if (isMounted) {
+                    setDevice(result);
+                    setIsOwner(result.userId == userId);
+                }
+            })
+            .catch((error) => {
+                setError(error.message)
+            });
+    
+        return () => {
+            isMounted = false;
+        };
+    }, [deviceId]);
+
+
+    console.log(isOwner);
+
+
 
     return (
+
+        <div>
+             {error && ( 
+                <div className={styles.errorContainer}>
+                    <strong>Error:</strong>
+                    <span>{error}</span>
+                </div>
+             )} 
+
         <div className="max-w-md mx-auto mt-8 p-4 border border-gray-300 rounded-md bg-white">
             <img
-                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC9zV2Kdykohn322cIp3e8Nk4u-n6ZHR0K1w&usqp=CAU'
+                src={device.imageUrl}
                 alt='ll'
                 className="w-full h-64 object-cover mb-4 rounded-md"
             />
@@ -11,29 +58,31 @@ const DetailsDevice = () => {
                 Brand Model
             </h2>
             <p className="text-gray-700 mb-2">
-                <span className="font-semibold">Type:</span> some type
+                <span className="font-semibold">Type:</span> {device.deviceType}
             </p>
             <p className="text-gray-700 mb-2">
-                <span className="font-semibold">Condition:</span> consdofn
+                <span className="font-semibold">Condition:</span> {device.condition}
             </p>
             <p className="text-gray-700 mb-2">
                 <span className="font-semibold">Storage Capacity:</span>{' '}
-                1T
+                {device.storageCapacity}
             </p>
             <p className="text-gray-700 mb-2">
-                <span className="font-semibold">Color:</span>pink
+                <span className="font-semibold">Color:</span>{device.color}
             </p>
             <p className="text-gray-700 mb-2">
-                <span className="font-semibold">Price:</span> $345
+                <span className="font-semibold">Price:</span> ${device.price}
             </p>
             <p className="text-gray-700 mb-4">
-                <span className="font-semibold">Description:</span> desctpskjtr 
+                <span className="font-semibold">Description:</span> {device.description} 
             </p>
             <p className="text-gray-700 mb-4">
                 <span className="font-semibold">Seller Number:</span>{' '}
-                0235235
+                {device.sellerNumber}
             </p>
         </div>
+
+        </div> 
     );
 
 }
