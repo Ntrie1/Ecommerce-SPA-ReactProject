06b4291069e2078c71d2b53c8e299fe8f66cc58a
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-12-03 20:45:45 +0200

    Update AllDevices.jsx

--- client/src/components/devices/all-devices/AllDevices.jsx
+++ client/src/components/devices/all-devices/AllDevices.jsx
@@ -4,17 +4,20 @@ import * as deviceService from '../../../services/devicesService'
 import OneDevice from "./one-device/OneDevice";
 
 import styles from './AllDevices.module.css'
+import Loader from "../../common/loader/Loader";
 
 const AllDevices = () => {
     const [devices, setDevices] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const abortController = new AbortController();
-    
+
         const fetchData = async () => {
             try {
-                const response = await deviceService.getAll( { signal: abortController.signal });
+                const response = await deviceService.getAll({ signal: abortController.signal });
                 setDevices(response);
+                setIsLoading(false);
             } catch (error) {
                 if (error.name === 'AbortError') {
                     console.log('Fetch aborted:', error.message);
@@ -23,9 +26,9 @@ const AllDevices = () => {
                 }
             }
         };
-    
+
         fetchData();
-    
+
         return () => {
             abortController.abort();
         };
@@ -33,26 +36,34 @@ const AllDevices = () => {
 
 
 
-    return(
-        <div className={styles.fullViewport}>
-
-        <div className={styles.customContainer}>
-        <h2 className="text-2xl font-semibold mb-4">All Devices</h2>
-        <div className={styles.deviceGrid}>
-    
-        {devices.map(device =>
-             <OneDevice
-             key={device._id} 
-             id={device._id}
-              imageUrl={device.imageUrl}
-              type={device.deviceType}
-              brand={device.brand}
-              price={device.price}
-              />)}
-    
-        </div>
-        </div>
-        </div>
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : devices.length === 0 ? (
+                <div className={`${styles.fullViewport} ${styles.customContainer} flex items-center justify-center`}>
+                    <p className="text-xl text-gray-800">No devices yet. Please try again later.</p>
+                </div>
+            ) : (
+                <div className={styles.fullViewport}>
+                    <div className={styles.customContainer}>
+                        <h2 className="text-2xl font-semibold mb-4">All Devices</h2>
+                        <div className={styles.deviceGrid}>
+                            {devices.map((device) => (
+                                <OneDevice
+                                    key={device._id}
+                                    id={device._id}
+                                    imageUrl={device.imageUrl}
+                                    type={device.deviceType}
+                                    brand={device.brand}
+                                    price={device.price}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
