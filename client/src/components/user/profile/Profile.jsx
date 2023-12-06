3c334567e97c977b70cf569767e22fd1d9bf2e43
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2023-12-06 12:25:31 +0200

    loader added

--- client/src/components/user/profile/Profile.jsx
+++ client/src/components/user/profile/Profile.jsx
@@ -5,10 +5,12 @@ import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../../context/authContext';
 
 import * as authService from '../../../services/authService';
+import Loader from '../../common/loader/Loader';
 
 
 const Profile = () => {
     const [devices, setDevices] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const { username, email } = useContext(AuthContext);
 
     useEffect(() =>{
@@ -17,7 +19,7 @@ const Profile = () => {
             try {
               const devicesData = await authService.createdDevices()
               .then((res) => {
-                if (isMounted) setDevices(res);
+                if (isMounted) setDevices(res), setIsLoading(false);
             })
             } catch (error) {
               console.error('Error fetching devices:', error);
@@ -39,7 +41,7 @@ const Profile = () => {
     return (
         <div className={styles.mainViewPort}>
         <div className="container mx-auto mb-8 pt-4">
-            {/* {userData && ( */}
+        
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-3xl font-bold mb-4">Your Profile</h1>
                 <div className="bg-white p-8 rounded shadow text-center mb-4">
@@ -47,45 +49,51 @@ const Profile = () => {
                     <p className="text-gray-600 mb-4">Email: {email}</p>
                 </div>
             </div>
-            {/* // )} */}
+        
 
             {/* {devices.length > 0 && ( */}
             <div>
-                <div className={styles.fullViewport}>
+             
+                  {isLoading ? (
+                    <Loader/>
+                  ) : (
+                    <div className={styles.fullViewport}>
                     <div className={styles.customContainer}>
 
-                        <h2 className={styles.heading}>Your offers:</h2>
+                    <h2 className={styles.heading}>Your offers:</h2>
 
-                      {devices.length > 0 ? (
-                          <div className={styles.deviceGrid}>
-                          {devices.map((device) => (
-                          <div key={device._id} className="bg-white p-4 shadow-md rounded-md">
-                              <img
-                                  src={device.imageUrl}
-                                  alt='ll'
-                                  className="w-full h-32 object-cover mb-4 rounded-md"
-                              />
-                              <h3 className="text-lg font-semibold mb-2">Type: {device.deviceType}</h3>
-                              <p className="text-gray-500 text-sm mb-2">Brand: {device.brand}</p>
-                              <p className="text-gray-700 text-lg font-bold mb-2">${device.price}</p>
-                              <Link to={`/devices/${device._id}`} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
-                                  Go to your offer
-                              </Link>
-                          </div>
-                           ))}
-                           </div>
-                      ) : (
-                        <div className="flex flex-col items-center justify-center bg-gray-100 p-8 rounded shadow">
-        <p className="text-4xl font-semibold mb-4">You haven't created any offers yet.</p>
-        <Link to="/devices/create" className="bg-yellow-700 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition duration-300">
-          Create an offer
-        </Link>
-      </div>
-                      )
-                      }
-                      
+                  {devices.length > 0 ? (
+                      <div className={styles.deviceGrid}>
+                      {devices.map((device) => (
+                      <div key={device._id} className="bg-white p-4 shadow-md rounded-md">
+                          <img
+                              src={device.imageUrl}
+                              alt='ll'
+                              className="w-full h-32 object-cover mb-4 rounded-md"
+                          />
+                          <h3 className="text-lg font-semibold mb-2">Type: {device.deviceType}</h3>
+                          <p className="text-gray-500 text-sm mb-2">Brand: {device.brand}</p>
+                          <p className="text-gray-700 text-lg font-bold mb-2">${device.price}</p>
+                          <Link to={`/devices/${device._id}`} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
+                              Go to your offer
+                          </Link>
+                      </div>
+                       ))}
+                       </div>
+                  ) : (
+                    <div className="flex flex-col items-center justify-center bg-gray-100 p-8 rounded shadow">
+    <p className="text-4xl font-semibold mb-4">You haven't created any offers yet.</p>
+    <Link to="/devices/create" className="bg-yellow-700 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition duration-300">
+      Create an offer
+    </Link>
+  </div>
+                  )
+                  }
+                  
                     </div>
                 </div>
+                  )}
+                    
             </div>
             {/* )} */}
         </div>
